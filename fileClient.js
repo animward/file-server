4a64f388b913b3251112cc6612fa47cf091f7337
EgoExperiments <egoexperiments@gmail.com>
2024-03-13 18:52:04 -0700

    more basics and outlined todo list for later

--- fileClient.js
+++ fileClient.js
@@ -1,1 +1,17 @@
+const net = require('net');
 
+const HOST = 'localhost';
+const PORT = 3000;
+
+const client = new net.Socket();
+
+client.connect(HOST, PORT, () => {
+    console.log('Connected to server');
+});
+
+// TODO
+
+/*
+request file,
+handle file data save it to local
+close connection*/
--- fileServer.js
+++ fileServer.js
@@ -4,5 +4,13 @@ const fs = require('fs');
 const server = net.createServer((socket) => {
     console.log('Client connected');
 
+    // TODO
+    /*
+    get filename as string
+    handle errors
+    file data
+    end*/
 
 })
+
+// listen port and log
